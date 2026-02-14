2ce2b2fee46ddb1e43a2f243b27631f32fa368f8
Liyabona Meyeliseli <liyaliabona@gmail.com>
2026-02-14 19:18:09 +0200

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-  });
-});
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    },
+    { threshold: 0.2 }
+);
+
+reveals.forEach(reveal => observer.observe(reveal));
